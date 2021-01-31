8dadfabb5e92c4ecbfb348986d389623f5d57694
Barbara Forbes <ba@4bes.nl>
2021-01-31 11:30:06 +0100

    initial commit

---
+++ New-Password.ps1
@@ -0,0 +1,58 @@
+<#
+    .SYNOPSIS
+    This script generates a password
+    .DESCRIPTION
+    A password is generated that contains at least one  non-capital, one capital, one number and one special character.
+    It will than removes the similar characters (like I & l)
+    .PARAMETER length
+    The length of the password
+    .EXAMPLE
+    New-Password -format Custom
+    .NOTES
+    Written by Barbara Forbes
+    @Ba4bes
+    https://4bes.nl
+    #>
+param(
+    [parameter(Mandatory = $true)]
+    [int]$Length
+)
+
+#Unix-16 codes are used to create numbers for all the needed characters
+$Charsets = @{
+    special      = @( (35..38), (40..47), (58..64) , (91..96), (123..126))
+    numbers      = 48..57
+    capitals     = 65..90
+    nonecapitals = 97..122
+}
+
+# An array is created, maximum length is set
+$PasswordArray = @()
+[int]$left = $charsets.Count
+[int]$Count = 0
+foreach ($Charset in $Charsets.GetEnumerator()) {
+    $Left--
+
+    # a random ammount of characters is chosen
+    if ($left -gt 0) {
+        $Count = 1..([int]($Length / 2) - $left ) | Get-Random
+    }
+    else {
+        $Count = $Length
+    }
+
+    $PasswordArray += (($Charset.value | Get-Random -Count $Count | ForEach-Object { [char]$_ }))
+    $Length = $Length - $Count
+}
+[string]$GeneratedPassword = -join ($PasswordArray | Get-Random -Count $PasswordArray.length)
+
+# Replace characters that can be confused with each other and characters that give issues in scripts
+$ReplacedCharacters = "[1IiLl\|Oo0\-_]+\&\|="
+
+while ($GeneratedPassword -match $ReplacedCharacters) {
+    $randomcharacter = [char]((65..90) + (97..122) | Get-Random -Count 1)
+    $GeneratedPassword = $GeneratedPassword -replace $Matches[0], $randomcharacter
+}
+
+# Return password to workflow
+echo "::set-output name=password::$GeneratedPassword"
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Password-Generator
+
+This action generates a random password.
+It will contain at least one non-capital, one capital, one number and one special character.
+It will not contain similar characters (like I & l)
+
+Note: There is a good chance of this string being exposed somewhere in the logs when another step uses it. So use with care! This is best suited for lab/demo purposes or temporary passwords.
+
+To use the created string, call the output in a separate action with the following syntax:
+```${{ steps.[stepID].outputs.password }}```
+
+## example usage
+
+```yml
+     - uses: actions/checkout@v2
+     - id: generatedpassword
+       uses: Ba4bes/Password-Generator@v0.1
+     - run: echo "${{ steps.generatedpassword.outputs.password }}"
+       shell: bash
+```
